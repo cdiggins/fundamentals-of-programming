fd1f66d58b311ebad9a0b271d8399eb961b435b5
Christopher Diggins <cdiggins@gmail.com>
2024-02-17 19:32:07 -0500

    Updating gitignore and package

--- fop/src/app/page.js
+++ fop/src/app/page.js
@@ -1,55 +1,197 @@
 'use client'
-import Image from "next/image";
-import React from 'react';
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import SendIcon from '@mui/icons-material/Send';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import { historyField } from '@codemirror/commands';
-import { Splitter, SplitterPanel } from 'primereact/splitter';
 
-// When custom fields should be serialized, you can pass them in as an object mapping property names to fields.
-// See [toJSON](https://codemirror.net/docs/ref/#state.EditorState.toJSON) documentation for more details
-const stateFields = { history: historyField };
+const drawerWidth = 240;
 
-export function EditorWithInitialState() {
-  const serializedState = localStorage.getItem('myEditorState');
-  const value = localStorage.getItem('myValue') || '';
 
-  return (
-    <CodeMirror
-      value={value}
-      initialState={
-        serializedState
-          ? {
-              json: JSON.parse(serializedState || ''),
-              fields: stateFields,
-            }
-          : undefined
-      }
-      onChange={(value, viewUpdate) => {
-        localStorage.setItem('myValue', value);
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
 
-        const state = viewUpdate.state.toJSON(stateFields);
-        localStorage.setItem('myEditorState', JSON.stringify(state));
-      }}
-    />
-  );
-}
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
 
-export default function Home() {
-  const [value, setValue] = React.useState("console.log('hello world!');");
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function PersistentDrawerLeft() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
   const onChange = React.useCallback((val, viewUpdate) => {
     console.log('val:', val);
     setValue(val);
   }, []);
 
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <main>
-      <h1 className="center p-8">Fundamentals of Programming</h1>   
-      <div className="flex flex-row">
-      <CodeMirror value={value} className="flex-1 p-4" height="200px" extensions={[javascript({ jsx: true })]} onChange={onChange} />     
-      <CodeMirror value={value} className="flex-1 p-4" height="200px" extensions={[javascript({ jsx: true })]} onChange={onChange} />     
-      </div>
-    </main>
-  
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            Fundamentals of Programming
+          </Typography>
+          <Button variant="contained" endIcon={<SendIcon />}>
+          Send
+        </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Item>
+            <CodeMirror height="80vh" extensions={[javascript({ jsx: true })]} onChange={onChange} />
+            </Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>
+              <Stack spacing={2}>
+                <Item>
+                  <Typography paragraph>Hello</Typography>                
+                </Item>
+                <Item>
+                <Typography paragraph>World</Typography>                
+                </Item>
+              </Stack>
+            </Item>
+          </Grid>
+        </Grid>
+      </Main>
+    </Box>
   );
 }
